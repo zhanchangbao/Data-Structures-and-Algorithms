1a8cf82f9a2193f7a59d39f3acd3c6607e807058
zhanchangbao <zhanchangbao@shdata.com>
2019-11-25 16:49:52 +0800

    新增思维导图笔记

--- src/main/java/complexityAnalysis/Case01.java
+++ src/main/java/complexityAnalysis/Case01.java
@@ -39,3 +39,29 @@ public class Case01 {
         return sum_1;
     }
 }
+
+
+//    int cal(int n) {
+//        int sum_1 = 0;
+//        int p = 1;
+//        for (; p < 100; ++p) {
+//            sum_1 = sum_1 + p;
+//        }
+//        int sum_2 = 0;
+//        int q = 1;
+//        for (; q < n; ++q) {
+//            sum_2 = sum_2 + q;
+//        }
+//
+//        int sum_3 = 0;
+//        int i = 1;
+//        int j = 1;
+//        for (; i <= n; ++i) {
+//            j = 1;
+//            for (; j <= n; ++j) {
+//                sum_3 = sum_3 +  i * j;
+//            }
+//        }
+//
+//        return sum_1 + sum_2 + sum_3;
+//    }
