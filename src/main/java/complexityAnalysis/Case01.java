dd122ca4270ec01c7d097f2e721cfaa10d20cf65
zhanchangbao <zhanchangbao@shdata.com>
2019-11-25 17:06:48 +0800

    新增复杂度分析加法法则

--- src/main/java/complexityAnalysis/Case01.java
+++ src/main/java/complexityAnalysis/Case01.java
@@ -34,34 +34,27 @@ public class Case01 {
 
     public static int cal3(int n) {
         int sum_1 = 0;
+        int p = 1;
+        for (; p < 100; p++) {
+            sum_1 = sum_1 + p;
+        }
 
+        int sum_2 = 0;
+        int q = 1;
+        for (; q < n; q++) {
+            sum_2 = sum_2 + q;
+        }
 
-        return sum_1;
+        int sum_3 = 0;
+        int i = 1;
+        int j = 1;
+        for (; i <= n; i++) {
+            j = 1;
+            for (; j <= n; j++) {
+                sum_3 = sum_3 + i * j;
+            }
+        }
+
+        return sum_1 + sum_2 + sum_3;
     }
 }
-
-
-//    int cal(int n) {
-//        int sum_1 = 0;
-//        int p = 1;
-//        for (; p < 100; ++p) {
-//            sum_1 = sum_1 + p;
-//        }
-//        int sum_2 = 0;
-//        int q = 1;
-//        for (; q < n; ++q) {
-//            sum_2 = sum_2 + q;
-//        }
-//
-//        int sum_3 = 0;
-//        int i = 1;
-//        int j = 1;
-//        for (; i <= n; ++i) {
-//            j = 1;
-//            for (; j <= n; ++j) {
-//                sum_3 = sum_3 +  i * j;
-//            }
-//        }
-//
-//        return sum_1 + sum_2 + sum_3;
-//    }
