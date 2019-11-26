d60ef83ecde46190c9d8240d45615e9c25384987
zhanchangbao <zhanchangbao@shdata.com>
2019-11-26 09:15:22 +0800

    新增数组操作

--- src/main/java/array/Array.java
+++ src/main/java/array/Array.java
@@ -82,18 +82,3 @@ public class Array {
         array.printAll();
     }
 }
-
-
-//    public static void main(String[] args) {
-//        Array array = new Array(5);
-//        array.printAll();
-//        array.insert(0, 3);
-//        array.insert(0, 4);
-//        array.insert(1, 5);
-//        array.insert(3, 9);
-//        array.insert(3, 10);
-//        //array.insert(3, 11);
-//        array.printAll();
-//    }
-//}
-
